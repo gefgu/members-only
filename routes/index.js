5bd53cb314ae00d81e20314314e7286b6eb135de
gefgu <gefgu@hotmail.com>
2022-07-12 15:46:34 -0300

    Add sanitization of post request of sign up

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const { body, validationResult, check } = require("express-validator");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -9,5 +10,35 @@ router.get("/", function (req, res, next) {
 router.get("/sign-up", (req, res) =>
   res.render("sign-up-form", { title: "Sign Up" })
 );
+
+router.post("/sign-up", [
+  body("firstName", "First Name Mmst be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("lastName", "Last Name must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("username", "Username must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  check("password", "Password must have a minimum of 8 characters").isLength({
+    min: 8,
+  }),
+  check(
+    "confirmPassword",
+    "Confirm Password field must have the same value as the password field"
+  )
+    .exists()
+    .custom((value, { req }) => value === req.body.password),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(req.body);
+    console.log(errors);
+    res.redirect("/");
+  },
+]);
 
 module.exports = router;
