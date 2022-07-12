f5e788afb7f11262c2548a0073e9314f00440d84
gefgu <gefgu@hotmail.com>
2022-07-12 15:34:06 -0300

    Add sign up form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Members Only' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Members Only" });
 });
+
+router.get("/sign-up", (req, res) =>
+  res.render("sign-up-form", { title: "Sign Up" })
+);
 
 module.exports = router;
