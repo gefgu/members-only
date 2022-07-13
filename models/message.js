4183e5a35523d316032138a8d36b4cb40a9ba659
gefgu <gefgu@hotmail.com>
2022-07-13 09:43:52 -0300

    Add author and date views for members

--- models/message.js
+++ models/message.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const { DateTime } = require("luxon");
+
 const MessageSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   timestamp: { type: Date, required: true },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+MessageSchema.virtual("date").get(function () {
+  return DateTime.fromJSDate(this.timestamp).toISODate();
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
